aacf3b1fe2fce39fc4281de7ccbf18cd6d806553
migs1750 <migdelacruz1@gmail.com>
2026-08-18 18:40:28 +0800

    Changes in progressions

--- progression.py
+++ progression.py
@@ -10,23 +10,19 @@ def arithmetic_sum(start, difference, n_term):
      for n in range(n_term):
         term = (n_term / 2) * (2 * start + (n_term - 1) * difference)   
      return term
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+def geometric_seq(start, ratio, n_term):
+    sequence = []
 
+    for n in range(n_term):
+        term = start * (ratio ** n)
+        sequence.append(term)
+    return sequence
+
+def geometric_sum(start, ratio, n_term):
+    if ratio == 1:
+        return n_term * start
+    else:
+        return start * (1 - ratio ** n_term) / (1 - ratio)
 
 print("Progression Calculator")
 
@@ -56,3 +52,20 @@ if choice == "arithmetic series":
 
     result = arithmetic_sum(first_term, common_difference, nth_term)
     print("The arithmetic series for that is" , result)
+
+if choice == "geometric":
+    first_term = int(input("Enter the first term: "))
+    common_ratio = int(input("Enter the common ratio: "))
+    nth_term = int(input("Enter the number of terms: "))
+
+    result = geometric_seq(first_term, common_ratio, nth_term)
+
+    print("Geometric sequence:", result)
+
+if choice == "geometric series":
+    first_term = int(input("Enter the first term: "))
+    common_ratio = int(input("Enter the common ratio: "))
+    nth_term = int(input("Enter the number of terms: "))
+
+    result = geometric_sum(first_term, common_ratio, nth_term)
+    print("The geometric series for that is" , result)
